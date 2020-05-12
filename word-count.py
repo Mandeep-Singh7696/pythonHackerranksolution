5391b939c3b497aaa6f4229bd314a4b959f7bea8
Mandeep-Singh7696 <45705551+Mandeep-Singh7696@users.noreply.github.com>
2020-05-12 20:57:19 +0530

    Add files via upload
    Word Count

---
+++ word-count.py
@@ -0,0 +1,11 @@
+l={}
+for i in range(int(input())):
+    x=input().lower()
+    if x not in l:
+        l[x]=1
+    else:
+        l[x]=l[x]+1
+print(len(l))
+for i in l:
+    print(l[i],end=" ") 
+
